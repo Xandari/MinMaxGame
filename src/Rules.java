31673a33454bc1a399a50ff42701a6cd65d21029
Xandari <kris.pieprz@gmail.com>
2023-12-04 21:18:18 +0100

    Refactor in progresss [WIP]

--- src/Rules.java
+++ src/Rules.java
@@ -84,79 +84,107 @@ public class Rules {
     String start() {
         Scanner scanner = new Scanner(System.in);
         var board = initBoard();
-int priority =random();
+        int priority = random();
+        String move;
+        player1 = "X";
+        botOrPlayer2 = "O";
+
         while (win == null) {
 
-            if (priority == 1) {
-                player1 = "X";
-                botOrPlayer2 = "O";
-
-                System.out.println(printBoard(board));
-
-                System.out.println("!! PLAYER TURN !!");
-                String move;
-                do {
-                    move = scanner.next();
-                    board = playerMoveValidation(move, board);
-                    if (!validated) {
-                        System.out.println("Wrong input");
-                    }
-                } while (!validated);
-
+            if (priority == 2) {
+                board = new BotMiniMax(botOrPlayer2, player1).computerMove(board);
+                System.out.println("!! BOT MOVED !!");
                 win = winCheck(board);
 
-                if (!"DRAW".equalsIgnoreCase(win)) {
-
-                    board = new BotMiniMax(player1, botOrPlayer2).computerMove(board);
-                    System.out.println("!! BOT MOVED !!");
-                    win = winCheck(board);
-                }
             } else {
-                player1 = "O";
-                botOrPlayer2 = "X";
+
                 System.out.println(printBoard(board));
+                System.out.println("!! PLAYER TURN !!");
 
-                if (!"DRAW".equalsIgnoreCase(win)) {
 
-                    board = new BotMiniMax(player1, botOrPlayer2).computerMove(board);
-                    System.out.println("!! BOT MOVED !!");
-                    win = winCheck(board);
+                move = scanner.next();
+                while (!playerMoveValidation(move, board)) {
+
+                    System.out.println("Wrong input");
+                    move = scanner.next();
                 }
 
-                System.out.println(printBoard(board));
-
-                System.out.println("!! PLAYER TURN !!");
-                String move;
-                do {
-                    move = scanner.next();
-                    board = playerMoveValidation(move, board);
-                    if (!validated) {
-                        System.out.println("Wrong input");
-                    }
-                } while (!validated);
-
+                board = playerMove(move, board);
                 win = winCheck(board);
 
             }
 
+
+            if (!"DRAW".equalsIgnoreCase(win)) {
+
+                board = new BotMiniMax(player1, botOrPlayer2).computerMove(board);
+                System.out.println("!! BOT MOVED !!");
+                win = winCheck(board);
+            }
+        } else{
+            player1 = "O";
+            botOrPlayer2 = "X";
+            System.out.println(printBoard(board));
+
+            if (!"DRAW".equalsIgnoreCase(win)) {
+
+                board = new BotMiniMax(player1, botOrPlayer2).computerMove(board);
+                System.out.println("!! BOT MOVED !!");
+                win = winCheck(board);
+            }
+
+            System.out.println(printBoard(board));
+
+            System.out.println("!! PLAYER TURN !!");
+            do {
+                move = scanner.next();
+                board = playerMoveValidation(move, board);
+                if (!validated) {
+                    System.out.println("Wrong input");
+                }
+            } while (!validated);
+
+            win = winCheck(board);
+
         }
-        if (win.equalsIgnoreCase("DRAW")) {
-            gameResoult = "!!AFTER HEROIC WAR NOOBODY WINS!!";
-        } else gameResoult = "!! AFTER HEROIC WAR " + win + " WIN THIS DUEL !!";
+
+    }
+        if(win.equalsIgnoreCase("DRAW"))
+
+    {
+        gameResoult = "!!AFTER HEROIC WAR NOOBODY WINS!!";
+
+    } else gameResoult ="!! AFTER HEROIC WAR "+win +" WIN THIS DUEL !!";
         return gameResoult;
+}
+
+    boolean playerMoveValidation(String move, String[][] boardToChange) {
+        final List<String> validMoves = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
+//var moveAsIntiger = Integer.parseInt(move); // Po to aby ograniczyć parseInt x2 - ale potrzeba 2x if
+        if (!move.matches("^[1-9]$")
+                && !validMoves.contains(boardToChange[(Integer.parseInt(move) - 1) / 9][(Integer.parseInt(move) - 1) % 9])) {
+            return false;
+        }
+        /*
+        if (!move.matches("^[1-9]$")). {
+            return false;
+        }
+        // Check if the cell is already occupied
+        int startY = (Integer.parseInt(move) - 1) / 9;
+        int startX = (Integer.parseInt(move) - 1) % 9;
+
+       return  !boardToChange[startY][startX].equalsIgnoreCase("X")
+                        && !boardToChange[startY][startX].equalsIgnoreCase("O") ;*/
+        return true;
     }
 
-    public String[][] playerMoveValidation(String move, String[][] boardToChange) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (move.matches("^[1-9]$") && boardToChange[i][j].equalsIgnoreCase(move)
-                        && !boardToChange[i][j].equalsIgnoreCase("X")
-                        && !boardToChange[i][j].equalsIgnoreCase("O")) {
-                    boardToChange[i][j] = player1;
-                    validated = true;
-                }
-            }
-        }
+    public String[][] playerMove(String move, String[][] boardToChange) {
+        var moveAsInt = Integer.parseInt(move);
+        int startY = (moveAsInt - 1) / 9;
+        int startX = (moveAsInt - 1) % 9;
+
+        boardToChange[startY][startX] = player1;
+
         return boardToChange;
     }
 
